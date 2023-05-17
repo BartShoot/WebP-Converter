06a871be60a725263ec1fd063e981ee189117b4f
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2023-05-18 00:38:25 +0200

    Add project files.

---
+++ WebP-Converter/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.IO;
+using System.Management.Automation;
+using System.Windows.Input;
+
+namespace WebP_Converter.ViewModels
+{
+	public partial class MainWindowViewModel : ViewModelBase
+	{
+		[ObservableProperty]
+		public string detectedImagesLabel;
+		[ObservableProperty]
+		public string folderDirectory;
+		private string[] allPNGFiles;
+		public string Greeting => "Welcome to Avalonia!";
+		public ICommand Convert { get; set; }
+		public ICommand OpenDirectory { get; set; }
+		public async void OpenDirectoryFunction()
+		{
+
+			OpenFolderDialog open = new();
+			FolderDirectory = await open.ShowAsync(new Window());
+			allPNGFiles = Directory.GetFiles(FolderDirectory, "*.png");
+			DetectedImagesLabel = $"You found {allPNGFiles.Length} files! {allPNGFiles[0]}";
+		}
+		
+		public void ConvertToWebP()
+		{
+			PowerShell ps = PowerShell.Create();
+			foreach (var img in allPNGFiles)
+			{
+				ps.AddCommand("cwebp").AddParameter("q", 80).AddArgument(Path.GetFileName(img)).AddParameter("o", Path.GetFileNameWithoutExtension(img)+".webp");
+			}
+			ps.Invoke();
+		}
+
+		public MainWindowViewModel()
+		{
+			OpenDirectory = new RelayCommand(OpenDirectoryFunction);
+			Convert = new RelayCommand(ConvertToWebP);
+		}
+	}
+}
